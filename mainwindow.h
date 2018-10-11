e970f9ecb51744f7e05cecd047d8bb89015521b5
Kieran <sporious@googlemail.com>
2018-10-11 14:16:54 +0100

    asd

--- mainwindow.h
+++ mainwindow.h
@@ -19,13 +19,8 @@ class MainWindow : public QMainWindow
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
   private slots:
-    void on_main_textEdit_textChanged();
 
-    void on_main_comboBox_currentIndexChanged(const QString &arg1);
 
-    void on_main_tableView_activated(const QModelIndex &index);
-
-    void on_pushButton_clicked();
 
     void on_main_pushButton_clicked();
 
